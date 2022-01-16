21572f587ebd1a34b71e51aca1d81720765900e4
Ben Leitch <benleitch17@gmail.com>
2022-01-15 16:12:28 -0800

    modified the playSound button

--- PlaySound_Button.py
+++ PlaySound_Button.py
@@ -6,7 +6,7 @@ class playSound_Button:
     width = 25
     height = 25
 
-    def __init__(self, x=725, y=0):
+    def __init__(self, x=725, y=0) -> None:
         self.image_path = "Resources/play_sound_button.png"
         self.x_pos = x
         self.y_pos = y
@@ -14,5 +14,13 @@ class playSound_Button:
         self._image = pygame.image.load(self.image_path)
         self._image = pygame.transform.scale(self._image, (playSound_Button.width, playSound_Button.height))
 
-    def draw(self, screen):
+        self.collision_box = pygame.Rect(self.x_pos, self.y_pos, playSound_Button.width, playSound_Button.height)
+
+    def is_pressed(self, mouse_x_pos, mouse_y_pos) -> bool:
+        if self.x_pos <= mouse_x_pos <= self.x_pos + playSound_Button.width:
+            if self.y_pos <= mouse_y_pos <= self.y_pos + playSound_Button.height:
+                return True
+        return False
+
+    def draw(self, screen) -> None:
         screen.blit(self._image, (self.x_pos, self.y_pos))
